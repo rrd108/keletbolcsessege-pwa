4bcb21276f43034a47fe4c06a9e5c058dfd4c608
rrd <rrd@1108.cc>
2021-04-08 17:08:05 +0200

    design fix for index

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
-    <button>Gauranga</button>
+    <NuxtLink to="/home">Gauranga</NuxtLink>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  transition: "slideUp",
+}
 </script>
 
 <style scoped lang="scss">
@@ -14,10 +16,15 @@ div {
   align-items: flex-end;
   justify-content: center;
   height: 100vh;
+  background-image: url('~/assets/background-logo.png');
+  background-size: 35rem;
+  background-repeat: no-repeat;
+  background-position: center center;
 
-  button {
+  a {
     background-color: #483a1d;
     color: #ede0c5;
+    text-decoration: none;
     font-size: 2rem;
     font-weight: 500;
     border: none;
@@ -25,5 +32,14 @@ div {
     padding: 0.5rem 2rem;
     margin-bottom: 6rem;
   }
+}
+
+.slideUp-enter-active,
+.slideUp-leave-active {
+  transition: transform 0.5s;
+}
+.slideUp-enter,
+.slideUp-leave-active {
+  transform: translateY(-100rem);
 }
 </style>
