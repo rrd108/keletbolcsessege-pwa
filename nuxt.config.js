dd3c57aeca134a7e20cdc006d742f198a0848d01
rrd <rrd@1108.cc>
2021-04-09 15:15:24 +0200

    colors and icons fixed

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,7 +32,7 @@ export default {
       '@nuxtjs/fontawesome',
       {
         icons: {
-          solid: ['faBookOpen', 'faCog', 'faLightbulb', 'faStar']
+          solid: ['faBookOpen', 'faCog', 'faPlayCircle', 'faVolumeUp']
         }
       }
     ],
--- pages/home.vue
+++ pages/home.vue
@@ -19,17 +19,20 @@
       </nav>
     </header>
     <main>
+
       <section>
         <h2>Napi bölcsesség</h2>
         <div><FontAwesomeIcon icon="book-open"></FontAwesomeIcon></div>
       </section>
-      <section>
-        <h2>Napi bölcsesség</h2>
-        <div><FontAwesomeIcon icon="book-open"></FontAwesomeIcon></div>
+
+      <section class="sound">
+        <h2>Bhagavad-gita</h2>
+        <div><FontAwesomeIcon icon="volume-up"></FontAwesomeIcon></div>
       </section>
-      <section>
-        <h2>Napi bölcsesség</h2>
-        <div><FontAwesomeIcon icon="book-open"></FontAwesomeIcon></div>
+
+      <section class="video">
+        <h2>Sivarama Swami</h2>
+        <div><FontAwesomeIcon icon="play-circle"></FontAwesomeIcon></div>
       </section>
     </main>
   </div>
@@ -70,8 +73,16 @@ main {
     background-color: #5ab1bb;
     color: #fff;
     border-radius: 0.5rem;
-    padding: 0.3rem 1rem;
-    margin-bottom: 1rem;
+    padding: 1rem;
+    margin-bottom: 1.5rem;
+
+    &.sound {
+      background-color: #094D92;
+    }
+
+    &.video {
+      background-color: #861388;
+    }
 
     h2 {
       font-size: 1.7rem;
