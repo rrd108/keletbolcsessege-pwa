2da232012942999d1006a41cd59b5b276c1ee959
rrd <rrd@1108.cc>
2021-04-23 14:26:14 +0200

    vue-touch added

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,7 +20,10 @@ export default {
   },
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [{ src: '@/plugins/youtube.js', ssr: false }],
+  plugins: [
+    { src: '@/plugins/youtube.js', ssr: false },
+    { src: '@/plugins/vue-touch', ssr: false }
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
